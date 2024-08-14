80dcfdb29782b34ee546e42f1e8155d9de78bbb5
ramak <ramaistalan@gmail.com>
2024-08-14 16:16:06 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+
+
+
+dotenv.config();
+connectDB();
+const app=express(); 
+app.get('/',(req,res)=>{
+    res.send("<h1>welcome to ecommerce app</h1>");
+});
+app.use(morgan('dev'));
+app.use(express.json());
+const port=process.env.port ;
+
+app.listen(port,()=>{
+    console.log(`server running ${process.env.DEV_MODE} on ${port}`.bgCyan.white)
+});
+
